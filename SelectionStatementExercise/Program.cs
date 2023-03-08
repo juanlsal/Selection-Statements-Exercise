afb131c9ec1cfe96335b3318b41164852a6674b9
juan lopez salamanca <juanlopezsalamanca1@gmail.com>
2023-03-08 12:37:44 -0600

    completed

--- SelectionStatementExercise/Program.cs
+++ SelectionStatementExercise/Program.cs
@@ -1,10 +1,101 @@
-﻿namespace SelectionStatementExercise
+﻿using System.ComponentModel.Design;
+
+namespace SelectionStatementExercise
 {
     public class Program
     {
+
+        //exercise 1
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var r = new Random();
+            int favNumber = r.Next(1, 10);
+            Console.WriteLine("guess my number its 1-10");
+            int userInput = 0;
+            while (userInput != favNumber)
+            {
+                userInput = Convert.ToInt32(Console.ReadLine());
+                if (userInput > favNumber)
+                {
+                    Console.WriteLine("too high");
+                }
+                if (userInput < favNumber)
+                {
+                    Console.WriteLine("too low");
+                }
+                if (userInput == favNumber)
+                {
+                    Console.WriteLine("You guessed it!");
+                    break;
+                }
+            }
+            
+     
+            //exercise 2
+            Console.WriteLine("What is your favorite school subject?");
+            string FavSub = Console.ReadLine();
+            switch (FavSub)
+            {
+                case "math":
+                    Console.WriteLine("math is cool!");
+                    break;
+            
+                case "science":
+                    Console.WriteLine("math is cool!");
+                    break;
+
+                case "history":
+                    Console.WriteLine("history is cool!");
+                    break;
+
+                case "english":
+                    Console.WriteLine("english is cool!");
+                    break;
+
+                case "lunch":
+                    Console.WriteLine("lunch is awesome!");
+                    break;
+
+                default:
+                    Console.WriteLine("Try again");
+                    Repeat();
+                    break;
+
+            }
+        }
+
+        static void Repeat()
+        {
+            Console.WriteLine("What is your favorite school subject?");
+            string FavSub = Console.ReadLine();
+            switch (FavSub)
+            {
+                case "math":
+                    Console.WriteLine("math is cool!");
+                    break;
+
+                case "science":
+                    Console.WriteLine("math is cool!");
+                    break;
+
+                case "history":
+                    Console.WriteLine("history is cool!");
+                    break;
+
+                case "english":
+                    Console.WriteLine("english is cool!");
+                    break;
+
+                case "lunch":
+                    Console.WriteLine("lunch is awesome!");
+                    break;
+
+                default:
+                    Console.WriteLine("Try again");
+                    Repeat();
+                    break;
+
+            }
         }
     }
 }
